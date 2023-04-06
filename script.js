0fd6bc66782fc57ca1bd03eef4f8c9ce93b32834
Nick <119486388+CryPaw@users.noreply.github.com>
2023-04-06 22:26:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const county2 = document.querySelector('.county2');
 const county3 = document.querySelector('.county3');
 const county4 = document.querySelector('.county4');
 const county5 = document.querySelector('.county5');
-const buttons = document.querySelectorAll('button');
+// const buttons = document.querySelectorAll('button');
 
   function count() {
     let counter = 0;
@@ -23,20 +23,36 @@ const buttons = document.querySelectorAll('button');
     const counter4 = count();
     const counter5 = count();
     
-    for (let button of buttons) {
-    button.addEventListener('click', function() {
-      if (button === mybtn1) {
-      county1.textContent = counter1();
-      } else if (button === mybtn2){
-        county2.textContent = counter2();
-      } else if (button === mybtn3){
-        county3.textContent = counter3();
-      } else if (button === mybtn4){
-        county4.textContent = counter4();
-      } else if (button === mybtn5){
-        county5.textContent = counter5();
-      }
-    });
-  }
+  //   for (let button of buttons) {
+  //   button.addEventListener('click', function() {
+  //     if (button === mybtn1) {
+  //     county1.textContent = counter1();
+  //     } else if (button === mybtn2){
+  //       county2.textContent = counter2();
+  //     } else if (button === mybtn3){
+  //       county3.textContent = counter3();
+  //     } else if (button === mybtn4){
+  //       county4.textContent = counter4();
+  //     } else if (button === mybtn5){
+  //       county5.textContent = counter5();
+  //     }
+  //   });
+  // }
 
-        
+
+document.getElementById("vot").addEventListener("click", function(event) {
+  
+  let targetElement = event.target;
+
+  if (targetElement === mybtn1) {
+      county1.textContent = counter1();
+    } else if (targetElement === mybtn2){
+      county2.textContent = counter2();
+    } else if (targetElement === mybtn3){
+      county3.textContent = counter3();
+    } else if (targetElement === mybtn4){
+      county4.textContent = counter4();
+    } else if (targetElement === mybtn5){
+      county5.textContent = counter5();
+    }
+});
